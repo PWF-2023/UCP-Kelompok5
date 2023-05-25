7ac98fbc8a0c72e39c153daf65f4e44cf2d79547
Salwasalsabilaa <azhar.salwa.ft202mail.umy.ac.id>
2023-05-26 02:31:37 +0700

    Update Models Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class category extends Model
 {
     use HasFactory;
     protected $fillable = [
         'tittle',
         'user_id',
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function todos()
     {
         return $this->hasMany(Todo::class);
